8e94defd0d89d92bacf9d3fb184e2860147ba04e
arianamador <arian@arianamador.com>
2015-03-14 21:45:37 -0400

    Adjusted tests, gemspec, and minitest helper

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,13 +1,3 @@
 $:.unshift File.expand_path('../../lib', __FILE__)
-$:.unshift File.expand_path('../../config', __FILE__)
 require 'select_pdf_api'
-
 require 'minitest/autorun'
-require 'webmock/minitest'
-require 'vcr'
-
-VCR.configure do |c|
-	c.allow_http_connections_when_no_cassette = false
-  c.cassette_library_dir = "test/fixtures/vcr"
-  c.hook_into :webmock
-end
--- test/test_select_pdf_api/test_api.rb
+++ test/test_select_pdf_api/test_api.rb
@@ -2,72 +2,59 @@ require './test/minitest_helper'
 
 # API test
 describe SelectPdfApi do
-	let(:valid_url) 	{'http://www.google.com'}
-	let(:invalid_url) { SelectPdfApi.new({url: 'invalid_url'}) }
+	let(:fixtures)			 {"../test/fixtures"}
+	let(:minimum_config) {"#{fixtures}/minimum-config.yml"}
+	let(:maximum_config) {"#{fixtures}/all-config.yml"}
+	let(:blank_config)   {"#{fixtures}/blank-config.yml"}
 
-	let(:default_config_api) { SelectPdfApi.new({url: valid_url})}
-	let(:minimum_config_api) { SelectPdfApi.new({url: valid_url,
-		config_file: '../test/fixtures/minimum-config.yml'})}
-	let(:maximum_config_api) { SelectPdfApi.new({url: valid_url,
-			config_file: '../test/fixtures/all-config.yml'})}
+	let(:select_pdf)  {SelectPdfApi.new({url: 'http://www.google.com'})}
+
+	before do
+		select_pdf.config.load_config(minimum_config)
+	end
 
 	def test_class_exists
-		assert default_config_api
-		default_config_api.must_be_instance_of SelectPdfApi
+		assert select_pdf
+		select_pdf.must_be_instance_of SelectPdfApi
 	end
 
 	def test_save_to_should_be_changable
-		original_filename = "/tmp/test_file_1.tmp"
 		modified_filename = "/tmp/test_file_9.tmp"
-
-		new_select = SelectPdfApi.new({url: valid_url, save_to: original_filename})
-		new_select.save_to.must_equal original_filename
-		new_select.save_to = modified_filename
-		new_select.save_to.must_equal modified_filename
+		select_pdf.save_to.must_equal "document.pdf"
+		select_pdf.save_to = modified_filename
+		select_pdf.save_to.must_equal modified_filename
 	end
 
 	def test_url_should_be_changable
 		modified_url = "http://mail.yahoo.com"
-
-		default_config_api.url.must_equal valid_url
-		default_config_api.url = modified_url
-		default_config_api.url.must_equal modified_url
+		select_pdf.url.must_equal 'http://www.google.com'
+		select_pdf.url = modified_url
+		select_pdf.url.must_equal modified_url
 	end
 
 	describe "#params" do
 		def test_params_exists
-			default_config_api.must_respond_to 'params'
+			select_pdf.must_respond_to 'params'
 		end
 
 		def test_it_should_build_params_for_all_options
-			maximum_config_api.params.must_equal "key=valid-key-123-67ad&margin_bottom=2pt&margin_left=1.25pt&margin_right=2pt&owner_password=owner567&page_orientation=Landscape&page_size=Letter&user_password=user123&url=http://www.google.com"
+			select_pdf.config.load_config(maximum_config)
+			select_pdf.params.must_equal "key=valid-key-123-67ad&margin_bottom=2pt&margin_left=1.25pt&margin_right=2pt&owner_password=owner567&page_orientation=Landscape&page_size=Letter&user_password=user123&url=http://www.google.com"
 		end
 
 		def test_it_should_build_params_for_minimum_options
-			minimum_config_api.params.must_equal "key=random-valid-api-123abc-345dbc&url=http://www.google.com"
+			select_pdf.config.load_config(minimum_config)
+			select_pdf.params.must_equal "key=random-valid-api-123abc-345dbc&url=http://www.google.com"
 		end
 	end
 
 	describe "#download" do
 		def test_download_exists
-			default_config_api.must_respond_to 'download'
+			select_pdf.must_respond_to 'download'
 		end
 
 		def test_it_fails_without_a_url
 			-> {SelectPdfApi.new.download}.must_raise SelectPdfApi::DownloadError
-		end
-
-		def test_it_fails_with_an_invalid_url
-			VCR.use_cassette('download_with_invalid_url', :record => :new_episodes) do
-				-> {invalid_url.download}.must_raise SelectPdfApi::DownloadError
-			end
-		end
-
-		def test_it_downloads_a_pdf
-			VCR.use_cassette('download', :record => :new_episodes) do
-				default_config_api.download
-				default_config_api.success?
-			end
 		end
 	end
 end
